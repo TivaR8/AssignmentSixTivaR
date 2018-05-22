ce3d83a3057806633acff56fd4445da2ee0c154d
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-05-21 20:48:25 -0400

    Initial commit

---
+++ AssignmentSixTivaR/AssignmentSixForm.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace AssignmentSixTivaR
+{
+    partial class frmAssignmentSix
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbUserAction = new System.Windows.Forms.GroupBox();
+            this.btnStay = new System.Windows.Forms.Button();
+            this.btnHit = new System.Windows.Forms.Button();
+            this.lblComputerTag = new System.Windows.Forms.Label();
+            this.lblUserTag = new System.Windows.Forms.Label();
+            this.mnuMenu = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniNewGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniReset = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblComputerOverallScore = new System.Windows.Forms.Label();
+            this.lblUserOverallScore = new System.Windows.Forms.Label();
+            this.lblRounds = new System.Windows.Forms.Label();
+            this.lblCompCardTotal = new System.Windows.Forms.Label();
+            this.lblUserCardTotal = new System.Windows.Forms.Label();
+            this.picDeck = new System.Windows.Forms.PictureBox();
+            this.grbRoundsInfo = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.grbUserAction.SuspendLayout();
+            this.mnuMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picDeck)).BeginInit();
+            this.grbRoundsInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grbUserAction
+            // 
+            this.grbUserAction.Controls.Add(this.btnStay);
+            this.grbUserAction.Controls.Add(this.btnHit);
+            this.grbUserAction.Location = new System.Drawing.Point(26, 133);
+            this.grbUserAction.Name = "grbUserAction";
+            this.grbUserAction.Size = new System.Drawing.Size(200, 100);
+            this.grbUserAction.TabIndex = 0;
+            this.grbUserAction.TabStop = false;
+            this.grbUserAction.Text = "Would you like another card or are you happy with the cards you have?";
+            // 
+            // btnStay
+            // 
+            this.btnStay.Location = new System.Drawing.Point(102, 57);
+            this.btnStay.Name = "btnStay";
+            this.btnStay.Size = new System.Drawing.Size(75, 23);
+            this.btnStay.TabIndex = 1;
+            this.btnStay.Text = "Stay";
+            this.btnStay.UseVisualStyleBackColor = true;
+            // 
+            // btnHit
+            // 
+            this.btnHit.Location = new System.Drawing.Point(21, 57);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(75, 23);
+            this.btnHit.TabIndex = 0;
+            this.btnHit.Text = "Hit";
+            this.btnHit.UseVisualStyleBackColor = true;
+            // 
+            // lblComputerTag
+            // 
+            this.lblComputerTag.AutoSize = true;
+            this.lblComputerTag.Location = new System.Drawing.Point(360, 251);
+            this.lblComputerTag.Name = "lblComputerTag";
+            this.lblComputerTag.Size = new System.Drawing.Size(52, 13);
+            this.lblComputerTag.TabIndex = 1;
+            this.lblComputerTag.Text = "Computer";
+            // 
+            // lblUserTag
+            // 
+            this.lblUserTag.AutoSize = true;
+            this.lblUserTag.Location = new System.Drawing.Point(49, 251);
+            this.lblUserTag.Name = "lblUserTag";
+            this.lblUserTag.Size = new System.Drawing.Size(26, 13);
+            this.lblUserTag.TabIndex = 2;
+            this.lblUserTag.Text = "You";
+            // 
+            // mnuMenu
+            // 
+            this.mnuMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMenu.Name = "mnuMenu";
+            this.mnuMenu.Size = new System.Drawing.Size(623, 24);
+            this.mnuMenu.TabIndex = 3;
+            this.mnuMenu.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniNewGame,
+            this.mniReset,
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniNewGame
+            // 
+            this.mniNewGame.Name = "mniNewGame";
+            this.mniNewGame.Size = new System.Drawing.Size(132, 22);
+            this.mniNewGame.Text = "New Game";
+            // 
+            // mniReset
+            // 
+            this.mniReset.Name = "mniReset";
+            this.mniReset.Size = new System.Drawing.Size(132, 22);
+            this.mniReset.Text = "Reset";
+            this.mniReset.Click += new System.EventHandler(this.mniReset_Click);
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(132, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // lblComputerOverallScore
+            // 
+            this.lblComputerOverallScore.AutoSize = true;
+            this.lblComputerOverallScore.Location = new System.Drawing.Point(15, 39);
+            this.lblComputerOverallScore.Name = "lblComputerOverallScore";
+            this.lblComputerOverallScore.Size = new System.Drawing.Size(102, 13);
+            this.lblComputerOverallScore.TabIndex = 4;
+            this.lblComputerOverallScore.Text = "Computer\'s Score: 0";
+            // 
+            // lblUserOverallScore
+            // 
+            this.lblUserOverallScore.AutoSize = true;
+            this.lblUserOverallScore.Location = new System.Drawing.Point(15, 68);
+            this.lblUserOverallScore.Name = "lblUserOverallScore";
+            this.lblUserOverallScore.Size = new System.Drawing.Size(72, 13);
+            this.lblUserOverallScore.TabIndex = 5;
+            this.lblUserOverallScore.Text = "Your Score: 0";
+            // 
+            // lblRounds
+            // 
+            this.lblRounds.AutoSize = true;
+            this.lblRounds.Location = new System.Drawing.Point(15, 12);
+            this.lblRounds.Name = "lblRounds";
+            this.lblRounds.Size = new System.Drawing.Size(48, 13);
+            this.lblRounds.TabIndex = 6;
+            this.lblRounds.Text = "Round 1";
+            // 
+            // lblCompCardTotal
+            // 
+            this.lblCompCardTotal.AutoSize = true;
+            this.lblCompCardTotal.Location = new System.Drawing.Point(466, 251);
+            this.lblCompCardTotal.Name = "lblCompCardTotal";
+            this.lblCompCardTotal.Size = new System.Drawing.Size(74, 13);
+            this.lblCompCardTotal.TabIndex = 7;
+            this.lblCompCardTotal.Text = "Card Total = 0";
+            // 
+            // lblUserCardTotal
+            // 
+            this.lblUserCardTotal.AutoSize = true;
+            this.lblUserCardTotal.Location = new System.Drawing.Point(152, 251);
+            this.lblUserCardTotal.Name = "lblUserCardTotal";
+            this.lblUserCardTotal.Size = new System.Drawing.Size(74, 13);
+            this.lblUserCardTotal.TabIndex = 8;
+            this.lblUserCardTotal.Text = "Card Total = 0";
+            // 
+            // picDeck
+            // 
+            this.picDeck.Image = global::AssignmentSixTivaR.Properties.Resources.Front;
+            this.picDeck.Location = new System.Drawing.Point(379, 277);
+            this.picDeck.Name = "picDeck";
+            this.picDeck.Size = new System.Drawing.Size(115, 130);
+            this.picDeck.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDeck.TabIndex = 9;
+            this.picDeck.TabStop = false;
+            // 
+            // grbRoundsInfo
+            // 
+            this.grbRoundsInfo.Controls.Add(this.lblRounds);
+            this.grbRoundsInfo.Controls.Add(this.lblComputerOverallScore);
+            this.grbRoundsInfo.Controls.Add(this.lblUserOverallScore);
+            this.grbRoundsInfo.Location = new System.Drawing.Point(232, 32);
+            this.grbRoundsInfo.Name = "grbRoundsInfo";
+            this.grbRoundsInfo.Padding = new System.Windows.Forms.Padding(0);
+            this.grbRoundsInfo.Size = new System.Drawing.Size(132, 100);
+            this.grbRoundsInfo.TabIndex = 10;
+            this.grbRoundsInfo.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AssignmentSixTivaR.Properties.Resources.Front;
+            this.pictureBox1.Location = new System.Drawing.Point(379, 413);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(115, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmAssignmentSix
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 509);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.grbRoundsInfo);
+            this.Controls.Add(this.picDeck);
+            this.Controls.Add(this.lblUserCardTotal);
+            this.Controls.Add(this.lblCompCardTotal);
+            this.Controls.Add(this.lblUserTag);
+            this.Controls.Add(this.lblComputerTag);
+            this.Controls.Add(this.grbUserAction);
+            this.Controls.Add(this.mnuMenu);
+            this.MainMenuStrip = this.mnuMenu;
+            this.Name = "frmAssignmentSix";
+            this.Text = "A Better Game of 21 By Tiva Rait";
+            this.grbUserAction.ResumeLayout(false);
+            this.mnuMenu.ResumeLayout(false);
+            this.mnuMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picDeck)).EndInit();
+            this.grbRoundsInfo.ResumeLayout(false);
+            this.grbRoundsInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbUserAction;
+        private System.Windows.Forms.Button btnStay;
+        private System.Windows.Forms.Button btnHit;
+        private System.Windows.Forms.Label lblComputerTag;
+        private System.Windows.Forms.Label lblUserTag;
+        private System.Windows.Forms.MenuStrip mnuMenu;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniNewGame;
+        private System.Windows.Forms.ToolStripMenuItem mniReset;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.Label lblComputerOverallScore;
+        private System.Windows.Forms.Label lblUserOverallScore;
+        private System.Windows.Forms.Label lblRounds;
+        private System.Windows.Forms.Label lblCompCardTotal;
+        private System.Windows.Forms.Label lblUserCardTotal;
+        private System.Windows.Forms.PictureBox picDeck;
+        private System.Windows.Forms.GroupBox grbRoundsInfo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ AssignmentSixTivaR/AssignmentSixForm.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Created by: Tiva Rait
+ * Created on: May-21-2018
+ * Created for: ICS3U Programming
+ * Assignment 6b A Better Game of 21
+ * This program is a better game of 21 that the user can play against the computer 
+*/
+
+namespace AssignmentSixTivaR
+{
+    public partial class frmAssignmentSix : Form
+    {
+        // Declare constant public variables
+        const int BLACKJACK = 21;
+        const int MIN_NUM = 1;
+        const int DIFF_BETWEEN_CARDS = 136;
+
+        // Declare global variables
+        int userOverallScore = 0;
+        int computerOverallScore = 0;
+        int roundsPlayed = 0;
+        bool computerTurn = false;
+        int maxNum;
+
+        // To place the picture boxes in the correct places 
+        int userLastPictureBoxX = 75;
+        int userLastPictureBoxY = 277;
+        int computerLastPictureBoxX = 379;
+        int computerLastPictureBoxY = 277;
+
+
+        // To make the random number generator
+        Random randomNumberGenerator = new Random();
+
+        // Create the Card Deck list that will be used
+        List<String> cardDeck = new List<String>();
+
+        // Create the users hand and the computers hand
+        List<String> userHand = new List<String>();
+
+        List<String> computerHand = new List<String>();
+
+
+
+        public frmAssignmentSix()
+        {
+            InitializeComponent();
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Function: ResetAll
+        // Input: None
+        // Output: void
+        // Description: This resets ALL of the data and scores in the game
+        public void ResetAll()
+        {
+            // Set all of the global variables back to original status
+            userOverallScore = 0;
+            lblUserOverallScore.Text = ("Your Score: " + userOverallScore);
+            computerOverallScore = 0;
+            lblComputerOverallScore.Text = ("Your Score: " + computerOverallScore);
+            roundsPlayed = 0;
+            lblRounds.Text = ("Round " + roundsPlayed);
+
+
+            // Add all of the Cards to the cardDeck 
+            cardDeck.Add("AH");
+            cardDeck.Add("AD");
+            cardDeck.Add("AC");
+            cardDeck.Add("AS");
+
+            cardDeck.Add("2H");
+            cardDeck.Add("2D");
+            cardDeck.Add("2C");
+            cardDeck.Add("2S");
+
+            cardDeck.Add("3H");
+            cardDeck.Add("3D");
+            cardDeck.Add("3C");
+            cardDeck.Add("3S");
+
+            cardDeck.Add("4H");
+            cardDeck.Add("4D");
+            cardDeck.Add("4C");
+            cardDeck.Add("4S");
+
+            cardDeck.Add("5H");
+            cardDeck.Add("5D");
+            cardDeck.Add("5C");
+            cardDeck.Add("5S");
+
+            cardDeck.Add("6H");
+            cardDeck.Add("6D");
+            cardDeck.Add("6C");
+            cardDeck.Add("6S");
+
+            cardDeck.Add("7H");
+            cardDeck.Add("7D");
+            cardDeck.Add("7C");
+            cardDeck.Add("7S");
+
+            cardDeck.Add("8H");
+            cardDeck.Add("8D");
+            cardDeck.Add("8C");
+            cardDeck.Add("8S");
+
+            cardDeck.Add("9H");
+            cardDeck.Add("9D");
+            cardDeck.Add("9C");
+            cardDeck.Add("9S");
+
+            cardDeck.Add("10H");
+            cardDeck.Add("10D");
+            cardDeck.Add("10C");
+            cardDeck.Add("10S");
+
+            cardDeck.Add("JH");
+            cardDeck.Add("JD");
+            cardDeck.Add("JC");
+            cardDeck.Add("JS");
+
+            cardDeck.Add("QH");
+            cardDeck.Add("QD");
+            cardDeck.Add("QC");
+            cardDeck.Add("QS");
+
+            cardDeck.Add("KH");
+            cardDeck.Add("KD");
+            cardDeck.Add("KC");
+            cardDeck.Add("KS");
+
+            // Acess the Start New Game Function
+            this.StartNewGame();
+        }
+
+        private void mniReset_Click(object sender, EventArgs e)
+        {
+            this.ResetAll();
+        }
+
+        // Function: StartNewGame
+        // Input: None
+        // Output: void
+        // Description: Starts a new round or "New game"
+        private void StartNewGame()
+        {
+            // Set computers turn to be false just to be safe
+            computerTurn = false;
+
+            // Remove items from userHand and computerHand
+            userHand.Clear();
+            computerHand.Clear();
+
+            ///////////////////////////////////////////////////////
+            //Need to Delete picture boxes and everything from previous rounds
+            ///////////////////////////////////////////////////////
+
+            // Loop that will make sure the User is dealt at least two cards
+            for (int counter = 0; counter <= 2; counter++)
+            {
+                this.DealUserNewCard();
+            }
+
+        }
+
+        // Function: DealUserNewCard
+        // Input: None
+        // Output: newUserCard
+        // Description: Selects the users new cards
+        private void DealUserNewCard()
+        {
+            // Local variables
+            int randomUserCard;
+            string newUserCard;
+
+            // Set the max num to be equal to how many cards are left in the deck
+            maxNum = cardDeck.Count();
+
+            // Randomly generate the index Number
+            randomUserCard = randomNumberGenerator.Next(MIN_NUM, maxNum + 1);
+
+            // Add the card from cardDeck to userHand 
+            newUserCard = cardDeck[randomUserCard];
+
+            userHand.Add(newUserCard);
+
+            // Delete the card from cardDeck
+            cardDeck.RemoveAt(randomUserCard);
+
+            // Calculate the user's cards together
+            this.CalculateUserCards(newUserCard);
+
+            // Display the card
+            this.DisplayCard(newUserCard);
+
+        }
+
+        private void CalculateUserCards (string cardValue)
+        {
+
+        }
+
+        private void DisplayCard(string newCard)
+        {
+            // Local variables 
+            int compPicX, compPicY;
+            int userPicX, userPicY;
+            // Set the appropriate places for the picture boxes and change the y value
+            if (computerTurn == true)
+            {
+                computerLastPictureBoxY = computerLastPictureBoxY + 136;
+            }
+            else
+            {
+                userLastPictureBoxY = userLastPictureBoxY + 136;
+            }
+
+            // Set them to their shortNames (Used for clarity)
+            userPicY = userLastPictureBoxY;
+            userPicX = userLastPictureBoxX;
+            compPicY = computerLastPictureBoxY;
+            compPicX = computerLastPictureBoxX;
+
+            // To display the card
+            if (newCard == "AH")
+            {
+                if (computerTurn == true)
+                {
+                    GeneratePictureBox(compPicX, compPicY);
+                }
+                else
+                {
+                    GeneratePictureBox(userPicX, userPicY);
+                }
+            }
+
+        }
+
+        // Function: 
+        // Input:
+        // Output:
+        // Description:
+        private void GeneratePictureBox(int x, int y)
+        {
+            // Dynamically generate a new picture box and a new point at the given location
+            PictureBox tmpPicCard = new PictureBox();
+            Point pntPic = new System.Drawing.Point(x, y);
+
+            // Set the location of the picturebox
+            tmpPicCard.Location = pntPic;
+
+            // Assign the image to the pictureBox 
+            tmpPicCard.Image = Properties.Resources.Front;
+
+            // stretch the image to the size of the picture box
+            //tmpPicCard.SizeMode
+        }
+
+    }
+}
